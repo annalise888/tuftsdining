3fbd3b551ce52b4c0f2dfa2c413374aeb40b5510
annalise888 <52852720+annalise888@users.noreply.github.com>
2021-04-29 17:04:27 -0400

    Update process.js

--- process.js
+++ process.js
@@ -24,22 +24,40 @@ http.createServer(function (req, res) {
 			pdata = qs.parse(pdata);
 			MongoClient.connect(url,{useUnifiedTopology:true},function(err, db) {
 				if (err) {
-					console.log("Connection err: " + err);
+					res.write("Connection err: " + err);
 				}
-        var dbo = db.db("tuftsdining");
-        var coll = dbo.collection("menu");
-        
-        getFood(pdata['foodname'],coll);
+				var dbo = db.db("tuftsdining");
+				var coll = dbo.collection("menu");
 
-        setTimeout(function(){ db.close(); console.log("Success!");}, 1000);
-        
+				var foods = getFood(pdata['foodname'],coll);
+				res.write(foods);
+
+				setTimeout(function(){ db.close(); console.log("Success!");}, 1000);
 			})
-			res.end();
+			
 		});
 	} else {
 		res.writeHead(200, {'Content-Type':'text/html'});
 		res.write("Unknown page request");
 		res.end();
 	}
+	res.end();
 }).listen(port);
+
+function getFood(foodName, coll) {
+	var query = {food:{$regex : ".*" + foodName + ".*"}}
+	coll.find(query).toArray(function(err,items) {
+		if (err) {
+			return ("Error: " + err);
+		} else if (items.length == 0) {
+			return ("No food being served with that name.");
+		} else {
+			var foods = "";
+			for (i=0; i < items.length; i++) {
+				foods += (items[i].food + " is being served at " + items[i].hall + " on " + items[i].longdate + "<br>");
+			}
+			return foods;
+		}
+	})
+}
 
